6622de722c9d7a651c6c97c7b527c2a5a7c0a3c7
Deeter Neumann <drneumann@uwalumni.com>
2024-08-21 14:28:46 -0700

    Average Speed

--- 08_numbers-and-math/08_06_runner.py
+++ 08_numbers-and-math/08_06_runner.py
@@ -1,3 +1,13 @@
 # If a runner runs 10 miles in 30 minutes and 30 seconds,
 # What is their average speed in kilometers per hour?
 # (Tip: 1 mile = 1.6 km)
+
+distance_km = 10 * 1.6
+
+time_hours = 30.5 / 60
+
+avg_speed = distance_km / time_hours
+
+print(avg_speed)
+
+
