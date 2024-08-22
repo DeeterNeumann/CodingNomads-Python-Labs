68f99764b6f1e32fe6026fcf455f9f0eb721572a
Deeter Neumann <drneumann@uwalumni.com>
2024-08-21 22:08:50 -0700

    Discovered longest string

--- 09_strings-and-text/09_01_longest.py
+++ 09_strings-and-text/09_01_longest.py
@@ -5,3 +5,10 @@ longest_german_word = "Donaudampfschifffahrtsgesellschaftskapitänskajütentürs
 longest_hungarian_word = "Megszentségteleníthetetlenségeskedéseitekért"
 longest_finnish_word = "Lentokonesuihkuturbiinimoottoriapumekaanikkoaliupseerioppilas"
 strong_password = "%8Ddb^ca<*'{9pur/Y(8n}^QPm3G?JJY}\(<bCGHv^FfM}.;)khpkSYTfMA@>N"
+
+print("Longest German Word is:", len(longest_german_word), "characters")
+print("Longest Hungarian Word is:", len(longest_hungarian_word), "characters")
+print("Longest Finnish Word is:", len(longest_finnish_word), "characters")
+print("Strong password is:", len(strong_password), "characters")
+
+print("The string", strong_password, "is the longest string.")
