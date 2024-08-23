cf88dc9a20c1601db3e78f51cf7f588aea76363b
Deeter Neumann <drneumann@uwalumni.com>
2024-08-23 10:36:02 -0700

    letters picked

--- 09_strings-and-text/09_07_pick_your_letters.py
+++ 09_strings-and-text/09_07_pick_your_letters.py
@@ -3,3 +3,7 @@
 # the necessary letters from the given string.
 
 word = "tweezers "
+
+
+sentence = word[1:3] + word[8:] + word[-2:-7:-2] + word[8:] + word[0] + word[6] + word[2:4] + word[7]
+print(sentence)
