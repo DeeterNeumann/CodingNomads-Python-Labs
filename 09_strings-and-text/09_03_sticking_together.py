4859d77eaf2e882486c478490f23466103d166bf
Deeter Neumann <drneumann@uwalumni.com>
2024-08-23 10:04:40 -0700

    concatenation

--- 09_strings-and-text/09_03_sticking_together.py
+++ 09_strings-and-text/09_03_sticking_together.py
@@ -12,3 +12,19 @@ gum = "gum"
 shoe = "shoe"
 song = "spice girls: wannabe"
 body_part = "your ear"
+
+stuck_fingers = glue + fingers
+print(stuck_fingers)
+
+sleuth = gum + shoe
+print(sleuth)
+
+atom = p + " " + n
+print(atom)
+
+body = body_part + " " + teeth + " " + fingers
+print(body)
+
+sticky = pb + " " + gum
+
+print(sticky)
