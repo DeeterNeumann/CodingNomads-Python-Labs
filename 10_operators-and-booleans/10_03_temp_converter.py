828c09b88b2ec4df937d3b3f8fa9b0845f6ca579
Deeter Neumann <drneumann@uwalumni.com>
2024-08-25 00:04:07 -0700

    temp converter

--- 10_operators-and-booleans/10_03_temp_converter.py
+++ 10_operators-and-booleans/10_03_temp_converter.py
@@ -2,3 +2,8 @@
 # ----------------------
 # Write the necessary code to convert a degree in Fahrenheit
 # to Celsius and print it to the console. Use variable names!
+
+f = int(input("Enter temperature in degrees Fahrenheit: "))
+celsius = ((5 / 9) * (f - 32))
+print(celsius)
+
