e4b635d3e623d8f3a7745a22bf908650a814a04c
Deeter Neumann <drneumann@uwalumni.com>
2024-08-27 00:13:32 -0700

    total vowels

--- 11_conditionals-and-loops/11_02_vowel_ipsum.py
+++ 11_conditionals-and-loops/11_02_vowel_ipsum.py
@@ -7,3 +7,12 @@ aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
 voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
 Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
 mollit anim id est laborum."""
+
+vowel = ["a" , "e" , "i" , "o" , "u" , "A" , "E" , "I" , "O" , "U"]
+total_num_vowels = 0
+
+for vowel in lorem_ipsum:
+    total_num_vowels += 1
+
+print(total_num_vowels)
+
