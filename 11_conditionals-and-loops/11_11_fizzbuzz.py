630840e38a579701c8168126d70779fdffbc2b29
Deeter Neumann <drneumann@uwalumni.com>
2024-08-29 19:14:48 -0700

    Fizzbuzz

--- 11_conditionals-and-loops/11_11_fizzbuzz.py
+++ 11_conditionals-and-loops/11_11_fizzbuzz.py
@@ -4,3 +4,18 @@
 # - If the number is divisible by 3, write `Fizz` instead of the number
 # - If the number is divisible by 5, write `Buzz` instead of the number
 # - If the number is divisible by both 3 and 5, write `FizzBuzz` instead of the number
+
+
+n = 15
+
+numbers = list(range(1, n + 1))
+print(numbers)
+
+if n % 3 == 0 and n % 5 == 0:
+    print("FizzBuzz")
+elif n % 3 == 0:
+    print("Fizz")
+elif n % 5 == 0:
+    print("Buzz")
+else:
+    print("Please enter a different number!")
