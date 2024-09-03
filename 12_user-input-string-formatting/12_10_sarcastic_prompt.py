c3a820650a0f1a7298d563c4f38bb4af0a812fc3
Deeter Neumann <drneumann@uwalumni.com>
2024-09-03 12:53:48 -0700

    all too honest

--- 12_user-input-string-formatting/12_10_sarcastic_prompt.py
+++ 12_user-input-string-formatting/12_10_sarcastic_prompt.py
@@ -1,2 +1,14 @@
 # Create a sarcastic program that asks a user for their honest opinion,
 # then prints the same sentence back to them in aLtErNaTiNg CaPs.
+
+opinion = input("What is an honest opinion you hold?: ")
+
+honesty = ""
+for char in range(len(opinion)):
+    if not char % 2:
+        honesty = honesty + opinion[char].upper()
+
+    else:
+        honesty = honesty + opinion[char].lower()
+
+print(str(honesty))
