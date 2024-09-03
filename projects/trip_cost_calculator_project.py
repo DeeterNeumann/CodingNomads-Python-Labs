dfa786b8fbecfc6d08b111c17fe748bc96b15065
Deeter Neumann <drneumann@uwalumni.com>
2024-09-03 14:06:48 -0700

    trip cost calc

---
+++ projects/trip_cost_calculator_project.py
@@ -0,0 +1,18 @@
+# Receive the following arguments from the user:
+
+# Kilometers to drive
+# Liters-per-kilometer usage of the car
+# Price per liter of fuel
+# Calculate the cost of the trip and display it to the user in the console. \
+# Apply some of the string formatting tricks that you learned about in this course section.
+
+import math
+
+distance = int(input("How many kilometers do you plan to drive?: "))
+lpk = int(input("How many liters-per-kilometer does the car get that you plan on taking on the drive?: "))
+price = float(input("What is the current cost of fuel (per liter)?: "))
+
+trip_cost = ((distance / lpk) * price)
+
+print(f"""The cost to drive {distance} kilometers in a car that gets {lpk} 
+liters-per-kilometer at a cost of ${price:.2f} per liter is ${trip_cost:.2f}.""")
