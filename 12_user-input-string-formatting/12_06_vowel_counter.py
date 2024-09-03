2292f7d46a39a50ad764ccec4bfdf37910f38e2a
Deeter Neumann <drneumann@uwalumni.com>
2024-09-02 21:03:58 -0700

    vowel counter

--- 12_user-input-string-formatting/12_06_vowel_counter.py
+++ 12_user-input-string-formatting/12_06_vowel_counter.py
@@ -1,2 +1,14 @@
 # Write a script that takes a string input from a user
 # and prints a total count of how often each individual vowel appeared.
+
+phrase = input("Type any phrase: ")
+
+vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+
+count = 0
+
+for char in phrase:
+    if char in vowels:
+        count += 1
+
+print(f"There are {count} vowels in the phrase '{phrase}'.")
