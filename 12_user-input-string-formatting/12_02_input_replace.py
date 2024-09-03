d7d99238250c0859bcd2323c8db8901127d68602
Deeter Neumann <drneumann@uwalumni.com>
2024-09-02 19:13:14 -0700

    letter replaced by symbol

--- 12_user-input-string-formatting/12_02_input_replace.py
+++ 12_user-input-string-formatting/12_02_input_replace.py
@@ -4,3 +4,16 @@
 # String input: more python programming please
 # Symbol input: §
 # Result: §ore python progra§§ing please
+
+phrase = input("Please type in any phrase: ")
+symbol = input("Please enter a symbol: ")
+
+letter = (f"{phrase[0]}")
+
+replaced = phrase.replace(letter, symbol)
+
+print(replaced)
+
+# the tired dog is sleeping throughout the night.
+
+# how would I need to write this code if the first letter of the phrase was capitalized?
