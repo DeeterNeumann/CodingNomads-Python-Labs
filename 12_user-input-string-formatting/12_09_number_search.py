dffdc06b09618483d2b6cc4c6c1857f815a9b9a2
Deeter Neumann <drneumann@uwalumni.com>
2024-09-03 11:54:57 -0700

    number found

--- 12_user-input-string-formatting/12_09_number_search.py
+++ 12_user-input-string-formatting/12_09_number_search.py
@@ -1,3 +1,15 @@
 # Ask your user for a number between 0 and 1,000,000,000.
 # Use a `while` loop to find the number. When the number is found,
 # exit the loop and print the number to the console.
+
+number = int(input("Please enter a number between 0 and 1,000,000,000: "))
+
+n = 0
+
+while n != number:
+    n += 1
+    break
+
+print(number)
+
+# Need to put upper and lower bound on this code given the range the user was given?
