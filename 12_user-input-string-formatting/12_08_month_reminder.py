0e281400e65198246637bea6375cb3261b1b020b
Deeter Neumann <drneumann@uwalumni.com>
2024-09-03 11:31:50 -0700

    number to month

--- 12_user-input-string-formatting/12_08_month_reminder.py
+++ 12_user-input-string-formatting/12_08_month_reminder.py
@@ -3,3 +3,35 @@
 # "January", "February", ... "December"
 # Print "Error" if the number from the user is not between 1 and 12.
 # Use a nested `if` statement.
+
+month_number = int(input("Please enter a number between 1 and 12 (inclusive): "))
+
+
+# months = [(1, "January"), (2, "February"), (3, "March"), (4, "April"), (5, "May"), (6, "June"), (7, "July"), (8, "August"), (9, "September"), (10, "October"), (11, "November"), (12, "December")]
+
+if month_number == 1:
+    print("January")
+elif month_number == 2:
+    print("February")
+elif month_number == 3:
+    print("March")
+elif month_number == 4:
+    print("April")
+elif month_number == 5:
+    print("May")
+elif month_number == 6:
+    print("June")
+elif month_number == 7:
+    print("July")
+elif month_number == 8:
+    print("August")
+elif month_number == 9:
+    print("September")
+elif month_number == 10:
+    print("October")
+elif month_number == 11:
+    print("November")
+elif month_number == 12:
+    print("December")
+else:
+    print("Error")
