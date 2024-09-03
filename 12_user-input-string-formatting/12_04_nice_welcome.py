a42b6fc31a45f0da62ebbb0ca1acc49867d2fa41
Deeter Neumann <drneumann@uwalumni.com>
2024-09-02 20:37:20 -0700

    welcome

--- 12_user-input-string-formatting/12_04_nice_welcome.py
+++ 12_user-input-string-formatting/12_04_nice_welcome.py
@@ -3,3 +3,10 @@
 # If a user enters more than one name, e.g. "firstname lastname",
 # then use only their first name to overstep some personal boundaries
 # in your welcome message.
+
+name = input("Hello! What is your name?: ")
+
+name = name.split()
+
+print(f"Welcome {name[0]}!")
+
