7838a99544b23c1e21b70183aec338d65201d866
Deeter Neumann <drneumann@uwalumni.com>
2024-09-10 11:42:10 -0700

    quit

--- 13_modules-and-automation/13_04_get_outta_here.py
+++ 13_modules-and-automation/13_04_get_outta_here.py
@@ -3,3 +3,10 @@
 # for input in an infinite `while` loop.
 # If the user enters the word "quit", you can exit the program
 # using a functionality provided by this module.
+
+import sys
+
+while True:
+    quit = input("Can't stop, won't stop: ")
+    if quit == "quit":
+        sys.exit()
