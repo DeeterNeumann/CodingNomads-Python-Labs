0c8ec2d6dd64a8fcf377701df09dd16ed9d1510b
Deeter Neumann <drneumann@uwalumni.com>
2024-09-10 09:46:10 -0700

    date and time

--- 13_modules-and-automation/13_02_the_time_please.py
+++ 13_modules-and-automation/13_02_the_time_please.py
@@ -1,2 +1,13 @@
 # Use a built-in Python module to tell you the current date and time.
 # Research online, so you can print it in a readable manner.
+
+import datetime
+
+now = datetime.datetime.now()
+
+print("now =", now)
+
+
+date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
+
+print(date_time)
