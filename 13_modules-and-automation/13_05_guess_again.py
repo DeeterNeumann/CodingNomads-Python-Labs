3be9086602670e2cea4df0b43478dab753a58036
Deeter Neumann <drneumann@uwalumni.com>
2024-09-10 12:38:17 -0700

    guess a number revised

--- 13_modules-and-automation/13_05_guess_again.py
+++ 13_modules-and-automation/13_05_guess_again.py
@@ -1,3 +1,22 @@
 # Re-create the guess-my-number game from scratch. Don't peek!
 # This time, give your players only a certain amount of tries 
 # before they lose.
+
+import random
+
+number = random.randint(1, 100)
+guess = None
+
+attempts = 10
+
+while guess != number and attempts > 0:
+    guess = input("Guess a number between 1 and 100: ")
+    guess = int(guess)
+    if guess == number:
+        print("Congratulations! You guessed the number!")
+        break
+    else:
+        print(f"Nope! Try again! You have {attempts - 1} attempts remaining.")
+    attempts -= 1
+
+print(f"The number was {number}.")
