12a1ca8cfa9519fa5dc13657029709aa4eb27457
Deeter Neumann <drneumann@uwalumni.com>
2024-09-14 11:27:23 -0700

    DnD updated

--- projects/DandD_command_line_game.py
+++ projects/DandD_command_line_game.py
@@ -8,11 +8,12 @@ print(f"Welcome {name}, to the command-line world of Dungeons and Dragons!\n")
 player_pos = "c"
 
 #possess sword?
-sword = False
+has_sword = False
 
 #dragon status
 dragon_dead = False
 
+# **while not dragon_dead**
 while dragon_dead == False:
 ## Present them with a choice between two doors.
     player_pos = "c"
@@ -21,40 +22,48 @@ while dragon_dead == False:
         ## If they choose the left door, they'll see an empty room
         if door_choice == "left":
             player_pos = "l"
-            print("You have entered into a dark and seemingly empty room.\n")
+            print("\nYou have entered into a dark and seemingly empty room.\n")
         ## If they choose the right door, then they encounter a dragon.
         if door_choice == "right":
             player_pos = "r"
-            print("You have entered into the lair of an ornery dragon!\n")
+            print("\nYou have entered into the lair of an ornery dragon!\n")
     while player_pos == "l":
         ## When in the seemingly empty room, they can choose to look around. If they do so, they will find a sword. They can choose to take it or leave it.
         if player_pos == "l":
-            look_around = input("Do you want to further explore the seemingly empty room? yes or no: \n")
+            look_around = input("\nDo you want to further explore the dark, empty room? yes or no: \n")
             if look_around == "yes":
-                take_sword = input("You have found a sword! Do you wish to take the sword? yes or no: \n")
+                take_sword = input("\nYou have found a sword! Do you wish to take the sword? yes or no: \n")
+                #**sword = take_sword == "yes"**
                 if take_sword == "yes":
-                    sword = True
+                    has_sword = True
                 else:
-                    sword = False
-            left_to_hall = input("Do you want to remain in this room or return to the corridor? Please enter stay or return: \n")
+                    has_sword = False
+            left_to_hall = input("\nDo you want to remain in this room or return to the corridor? Please enter stay or return: \n")
             if left_to_hall == "return":
                 player_pos = "c"
+            elif left_to_hall == "stay" and has_sword:
+                print("You have found everything in this room.")
             else:
-                print("Well, this is fun.\n")
+                print("\nWell, this is fun.\n")
                 continue
     while player_pos == "r":
         ## When encountering the dragon, they have the choice to fight it.
         if player_pos == "r":
-            fight_safety = input("Do you wish to fight the dragon or return to the safety of the corridor? Enter fight or safety: \n")
-            if fight_safety == "fight" and sword == True:
+            fight_safety = input("\nDo you wish to fight the dragon or return to the safety of the corridor? Enter fight or safety: \n")
+            if fight_safety == "fight" and has_sword == True:
                 dragon_dead = True
                 break
         # If they don't have the sword, then they will be eaten by the dragon and lose the game
-            elif fight_safety == "fight" and sword == False:
-                print("You made a poor decision. Fighting the dragon without a weapon resulted in your death. You lose.\n")
+            elif fight_safety == "fight" and has_sword == False:
+                print("\nYou made a poor decision. Fighting the dragon without a weapon resulted in your death. You lose.\n")
                 quit()
             elif fight_safety == "safety":
                 player_pos = "c"
-                print("You safely regressed back to the corridor and out of danger from the dragon.\n")
+                print("\nYou safely regressed back to the corridor and out of danger from the dragon.\n")
 
-print("You found the sword and chose to fight the dragon, which you successfully slayed. Congratulations - you win!")
+print("\nYou found the sword and chose to fight the dragon, which you successfully slayed. Congratulations - you win!\n")
+
+# sword == True can be replaced by just sword
+# sword == False can be replaced by just not sword
+
+ # notice that if I take the sword, I can still look for and take the sword. Maybe you could use the sword flag to display a message that we already found the sword when in this situation
